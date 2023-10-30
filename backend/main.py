bd74cfdd3736a97b190b64b9615d34bd79e9b838
Izcoatl <izcoatl.ramirez4303@alumnos.udg.mx>
2023-10-29 20:12:50 -0600

    se corrige problema de freeze del servidor

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from routes.prediction import predict
-
 app = FastAPI()
+
 
 @app.get('/')
 def welcome():
-    return {'message':'welcome to fastAPI'}
+    return {'message': 'welcome to fastAPI'}
+
 
 app.include_router(predict)
--- backend/routes/prediction.py
+++ backend/routes/prediction.py
@@ -1,21 +1,34 @@
-from fastapi import APIRouter,HTTPException
+from fastapi import APIRouter, HTTPException, Request
+from models.image import Image
+from models.item import Item
 from fastapi.responses import JSONResponse
-from modelo.modelo import get_predict
-import os 
+from modelo.modelo import Model
+import os
 
 predict = APIRouter()
 
+model = Model()
+
+
 @predict.post('/api/predict')
-async def get_prediction():
-    img_filename = "gatomontes.jpg"  # Nombre de la imagen
-    img_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), img_filename)
-    
-    data = await get_predict(img_path=img_path)
-   
+async def get_prediction(img: Image):
+
+    img_filename = img.path  # Nombre de la imagen
+    img_path = os.path.join(os.path.abspath(
+        os.path.dirname(__file__)), img_filename)
+
+    data = await model.get_predict(img_path=img_path)
+
     if data:
+        print(data)
         return JSONResponse(content=data)
 
-    raise HTTPException(400,'Something went wrong')
-    
+    raise HTTPException(400, 'Something went wrong')
 
 
+@predict.post('/api/ejemplo')
+async def example(item: Item):
+    return {
+        item.title,
+        item.id,
+        item.description}
